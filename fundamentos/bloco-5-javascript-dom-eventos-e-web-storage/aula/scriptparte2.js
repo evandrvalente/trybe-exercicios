ef5f89c0b8c44ecbab7992278b132135b835f58e
evandrvalente <evandr.valente@gmail.com>
2021-12-08 12:35:11 -0300

    exercícios do dia 1 do bloco 5

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/aula/scriptparte2.js
@@ -0,0 +1,8 @@
+let emergency = document.querySelector('.emergency-tasks');
+emergency.style.background = 'pink';
+let emergencyTitle = document.querySelector('h3');
+emergencyTitle.style.background = 'purple';
+
+let noEmergency = document.querySelector('.no-emergency-tasks');
+noEmergency.style.background = 'yellow';
+
