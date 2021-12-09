71a2ff1d79febe22fccfbe95f3fabe2f91be559e
evandrvalente <evandr.valente@gmail.com>
2021-12-09 13:46:00 -0300

    exercícios do dia 1 do bloco 5 concluídos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scriptparte2.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scriptparte2.js
@@ -1,8 +1,13 @@
 let emergency = document.querySelector('.emergency-tasks');
 emergency.style.background = 'pink';
-let emergencyTitle = document.querySelector('h3');
+let emergencyTitle = document.getElementsByClassName('title-emergency')[0];
 emergencyTitle.style.background = 'purple';
+let emergencyTitle2 = document.getElementsByClassName('title-emergency')[1];
+emergencyTitle2.style.background = 'purple';
 
 let noEmergency = document.querySelector('.no-emergency-tasks');
 noEmergency.style.background = 'yellow';
-
+let emergencyTitle3 = document.getElementsByClassName('title-noemergency');
+for (let index in emergencyTitle3) {
+    emergencyTitle3[index].style.background = 'black';
+};
