0c1cd513bffc48b60104e6b66c223f3938cc1673
evandrvalente <evandr.valente@gmail.com>
2022-06-21 16:22:52 -0300

    Exercicio 5

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -2,12 +2,12 @@ const readline = require('readline-sync');
 // const altura = 1.6;
 // const peso = 63;
 
-const peso = readline.questionInt('Qual o seu peso? (em kg)');
+const peso = readline.questionFloat('Qual o seu peso? (em kg)');
 const altura = readline.questionInt('Qual a sua altura? (em cm)');
 
-const imc = (height, weight) => console.log(`Imc: ${weight/((height/100)^2)}`);
+const imc = peso/((altura/100)^2);
 
-const imcResult = console.log((imc(altura,peso)).toFixed(2));
+console.log(`IMC:  ${imc.toFixed(2)}`);
 
 // module.exports = imcResult;
 
